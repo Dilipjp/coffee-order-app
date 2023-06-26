afcffeba5394bf8f1fd486a8cb88dc1b4d604a66
Dilip <jpdilipk@gmail.com>
2023-06-27 05:05:58 +0530

    back button added to order page

--- app/src/main/java/com/coffee/shop/OrderActivity.java
+++ app/src/main/java/com/coffee/shop/OrderActivity.java
@@ -1,14 +1,35 @@
 package com.coffee.shop;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class OrderActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Orders");
+
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24); // Replace with your custom back arrow icon
+        }
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
