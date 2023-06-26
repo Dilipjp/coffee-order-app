00cf47ec71ea461e0d2cf3e43e290990f971e26d
Dilip <jpdilipk@gmail.com>
2023-06-27 04:16:57 +0530

    Home page designed

--- app/src/main/java/com/coffee/shop/MainActivity.java
+++ app/src/main/java/com/coffee/shop/MainActivity.java
@@ -1,14 +1,85 @@
 package com.coffee.shop;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    Button btn_logout;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Coffee Selection");
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation item clicks
+            switch (item.getItemId()) {
+                case R.id.menu_profile:
+                    // Open the profile page
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                    break;
+                case R.id.menu_orders:
+                    // Open the orders page
+                    startActivity(new Intent(MainActivity.this, OrderActivity.class));
+                    break;
+                // Add more cases for other menu items if needed
+            }
+
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+//        btn_logout = findViewById(R.id.btn_logout);
+//        textView = findViewById(R.id.useremail);
+
+//        if (currentUser == null) {
+//            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        } else {
+//            textView.setText(currentUser.getEmail());
+//        }
+
+//        btn_logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//
+//            }
+//        });
     }
 }
